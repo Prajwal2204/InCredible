380c95c3866c1b06d6e489f1b3d47a5b397723fd
Prajwal Navada <44873030+Prajwal2204@users.noreply.github.com>
2021-04-01 13:12:47 +0530

    admin front

--- admin.php
+++ admin.php
@@ -1,49 +1,68 @@
+<?php
+session_start();
+include_once 'config.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>InCredible</title>
-	<link rel="stylesheet" href="css/custom.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "css/styles-admin.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<body>
 
-<nav class="navbar navbar-default" role="navigation">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="admin.php">Admin Dashboard</a>
-		</div>
-		<div class="collapse navbar-collapse" id="navbar1">
-			<ul class="nav navbar-nav navbar-right">
 
-				
-				<li><a href="logout.php">Log Out</a></li>
-				<li><a href="login.php">Login</a></li>
-			</ul>
-		</div>
-	</div>
-</nav>
+<body class = "signin-body">
+	<nav>
+		<!-- <div class="container-fluid"> -->
+			<!-- <div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="admin.php">Admin Dashboard</a>
+			</div> -->
+			<!-- <div class="collapse navbar-collapse" id="navbar1"> -->
+				<ul class="topnav">
+					<li class = "logo"><img class = "logo-mod" src="img/logo.PNG"></li>
+					<li><a href="admin.php">Admin Dashboard</a></li>
+					<?php if (isset($_SESSION['usr_id'])) { ?>
+					<li class = "topnav-right"><a href="logout.php">Log Out</a></li>
+					<?php } else { ?>
+					<li class = "topnav-right"><a href="login.php">Login</a></li>
+					<?php } ?>
+				</ul>
+			<!-- </div> -->
+		<!-- </div> -->
+	</nav>
 
-<div style="width: 50px; height: 50px;"></div>
-<div class="col-lg-2">
-	<ul class="navbar navbar-default nav" style="height: 650px;">
+<!-- <div style="width: 50px; height: 50px;"></div> -->
+	<div class = "row">
+	<div class="col-3 sidenav">
+	<div style="width: 50px; height: 50px;"></div>
+		<ul class="list-unstyled">
 
-		<li><a href="addcard.php"><span style="margin-left: 25px; margin-top:20px; font-size: 20px;"><b>Add Card</b></span></a></li>
-		<li><a href="removecard.php"><span style="margin-left: 25px; margin-top: 20px; font-size: 20px;"><b>Remove Card</b></span></a></li>
-		<!-- <li><a href="grantloan.php"><span style="margin-left: 25px; margin-top: 20px; font-size: 20px;"><b>Grant Loan</b></span></a></li> -->
-		<li><a href="viewaccounts.php"><span style="margin-left: 25px; margin-top: 20px; font-size: 20px;"><b>View accounts</b></span></a></li>
-		<li><a href="depositmoney.php"><span style="margin-left: 25px; margin-top: 20px; font-size: 20px;"><b>Deposit money</b></span></a></li>
-		<li><a href="withdrawmoney.php"><span style="margin-left: 25px; margin-top: 20px; font-size: 20px;"><b>Withdraw Money</b></span></a></li>
-		<!-- <li><a href="viewloans.php"><span style="margin-left: 25px; margin-top: 20px; font-size: 20px;"><b>View loans</b></span></a></li> -->
+			<li><a class="btn btn-outline-danger" href="addcard.php"><b>Add Card</b></a></li>
+			<li><a class="btn btn-outline-danger" href="removecard.php"><b>Remove Card</b></a></li>
+			<li><a class="btn btn-outline-danger" href="viewaccounts.php"><b>View accounts</b></a></li>
+			<li><a class="btn btn-outline-danger" href="depositmoney.php"><b>Deposit money</b></a></li>
+			<li><a class="btn btn-outline-danger" href="withdrawmoney.php"><b>Withdraw Money</b></a></li>
 		</ul>
-		</div>
+	</div>
+	<div class = "row"></div>
+	<div class="col-8 custom-jumbo">
+		<div class="row">
+                <div class="col-sm-12">
+                    <h1 class="display-3"><img class="jumbo-img" src="img/logo-dark.PNG"></h1>
+                    <br>
+                    <h3 style="color: rgb(246,76,114);">A web app to take care of all your credit card needs.</h3>
+                    <br>
+                </div>
+            </div>
+	</div>
+	</div>
 
 </body>
 </html>
