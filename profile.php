97241a048b64a032c7551ba0fc96bba9d86e83d9
Prajwal Navada <44873030+Prajwal2204@users.noreply.github.com>
2021-04-11 19:12:51 +0530

    check

--- profile.php
+++ profile.php
@@ -132,7 +132,8 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                     </tr>
                     </tbody>
                             ';
-            }
+            }  
+			// sample check
                             
             echo'</table>';
                 ?>
